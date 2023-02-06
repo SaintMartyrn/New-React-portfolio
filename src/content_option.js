7c948e4bff47b8d7f4417e524b4a767e1dbf7281
SaintMartyrn <dartagnanhickey@.com>
2023-02-06 11:37:40 -0800

    fixed contact me

---
+++ src/content_option.js
@@ -0,0 +1,149 @@
+const logotext = "D'Artagnan";
+const meta = {
+    title: "Darts World",
+    description: "I’m D'Artagnan, a Full stack devloper based in Los Angeles California",
+};
+
+const introdata = {
+    title: "I’m D'Artagnan",
+    animated: {
+        first: "I love to code",
+        second: "Front end design is my jam",
+        third: "I can can develop mobile apps too!",
+    },
+    description: "Hello I am D'Artagnan Hickey and I am a fullstack developer from Los Angeles California. i am skilled in the use of both front end and back end technologies and look forward to increasing my knowledge in coding!",
+    // your_img: <img src={assets/images/heroImage.png} alt="Trees"  />
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: "I am a fullstack developer from Los Angeles, California. I love UI and UX design but am also proffecient with back end technologies as well. In my free time I enjoy cycling, videos games, and reading mangas.",
+};
+const worktimeline = [{
+}
+];
+
+const skills = [{
+        name: "html",
+        value: 90,
+    },
+    {
+        name: "css",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 85,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Jquery",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "As a painter and artist I became profecient working with technologies like tailwind, bootstrap, react, etc to create dynamic designs for any type of web page",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Using my knowledge of front end and back end technologies, I work towards creating engaging applications for mobile use as well as for desktops.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "dartagnanhickey@gmail.com",
+    YOUR_FONE: "(323)251-4506",
+    description: "These are the best ways you can reach me!",
+    // finish making my emailjs.com account 
+    // tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/SaintMartyrn",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
